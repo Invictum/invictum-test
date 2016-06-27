906c3ed8b86d5c3f63b148afb1c37d188c49fdc8
Iaroslav Iershov <Iaroslav_Iershov@epam.com>
2016-06-27 20:21:47 +0300

    Fixed issue with NPE of non existing resource loading.  - wrapped NPE in IllegalStateException.

--- src/main/java/com/github/invictum/utils/ResourceProvider.java
+++ src/main/java/com/github/invictum/utils/ResourceProvider.java
@@ -2,15 +2,23 @@ package com.github.invictum.utils;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.Collection;
 
 public class ResourceProvider {
 
+    private final static Logger LOG = LoggerFactory.getLogger(ResourceProvider.class);
+
     @SuppressWarnings("all")
     private static File getRoot(String rootPath) {
-        return new File(Thread.currentThread().getContextClassLoader().getResource(rootPath).getFile());
+        try {
+            return new File(Thread.currentThread().getContextClassLoader().getResource(rootPath).getFile());
+        } catch (RuntimeException e) {
+            throw new IllegalStateException(String.format("Failed to detect root at %s", rootPath));
+        }
     }
 
     public static File getFile(final String root, final String fileName) {
