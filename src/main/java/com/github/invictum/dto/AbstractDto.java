25d8372f3e52fb51203dafa34c8176832b7d587a
Illia Grynov <eliogrin@gmail.com>
2016-09-04 18:47:51 +0200

    updated: fix merge conflicts

--- src/main/java/com/github/invictum/dto/AbstractDto.java
+++ src/main/java/com/github/invictum/dto/AbstractDto.java
@@ -20,6 +20,35 @@ public class AbstractDto {
     public static final boolean FULL_DTO_VIEW = Boolean
             .valueOf(PropertiesUtil.getProperty(EnhancedSystemProperty.FullDtoView));
 
+    @Override
+    public String toString() {
+        String stringView = StringUtils.EMPTY;
+        List<Attribute> data = getData(this, FULL_DTO_VIEW);
+        if (data.isEmpty()) {
+            return "{null}";
+        }
+        for (Attribute attribute : data) {
+            String value = attribute.getValue();
+            stringView += String.format("%s: %s, ", attribute.getName(), value == null ? "null" : value);
+        }
+        return String.format("{%s}", stringView.substring(0, stringView.length() - 2));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+        List<Attribute> expected = getData(obj, false);
+        List<Attribute> actual = getData(this, false);
+        return actual.containsAll(expected);
+    }
+
+    @Override
+    public int hashCode() {
+        return getData(this, true).hashCode();
+    }
+
     private String extractData(Object object, Field attribute) {
         attribute.setAccessible(true);
         Object value;
@@ -52,64 +81,6 @@ public class AbstractDto {
             return value.toString();
         }
         LOG.debug("Unknown type of variable, for field {}.", attribute.getName());
-        return null;
-    }
-
-    private List<Attribute> getData(Object object, boolean includeNulls) {
-        List<Attribute> data = new ArrayList<>();
-        for (Field field : object.getClass().getDeclaredFields()) {
-            Attribute attribute = new Attribute();
-            if (field.isAnnotationPresent(DtoAttribute.class)) {
-                attribute.setName(field.getName());
-                attribute.setValue(extractData(object, field));
-                if (attribute.getValue() != null || includeNulls) {
-                    if (field.isAnnotationPresent(KeyAttribute.class)) {
-                        data.add(0, attribute);
-                    } else {
-                        data.add(attribute);
-                    }
-                }
-            }
-        }
-        return data;
-    }
-
-    @Override
-    public String toString() {
-        String stringView = StringUtils.EMPTY;
-        List<Attribute> data = getData(this, FULL_DTO_VIEW);
-        if (data.isEmpty()) {
-            return "{null}";
-        }
-        for (Attribute attribute : data) {
-            String value = attribute.getValue();
-            stringView += String.format("%s: %s, ", attribute.getName(), value == null ? "null" : value);
-        }
-        return String.format("{%s}", stringView.substring(0, stringView.length() - 2));
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj.getClass() != this.getClass()) {
-            return false;
-        }
-        List<Attribute> expected = getData(obj, false);
-        List<Attribute> actual = getData(this, false);
-        return actual.containsAll(expected);
-    }
-
-    @Override
-    public int hashCode() {
-        return getData(this, true).hashCode();
-    }
-
-    private String extractData(Object object, Field attribute) {
-        attribute.setAccessible(true);
-        try {
-            return (String) attribute.get(object);
-        } catch (IllegalAccessException e) {
-            LOG.error("Failed to get data for {} attribute", attribute);
-        }
         return null;
     }
 
