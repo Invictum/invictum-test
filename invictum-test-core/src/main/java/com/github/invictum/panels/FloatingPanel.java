7d23b2cbb96cc158f15106cb8b55aec47b51eb2f
Iaroslav Iershov <zim182@gmail.com>
2016-10-04 15:13:21 +0300

    CR changes: Rework locators usage strategy (#45).  - removed duplicated method in Floating Panel class.

--- invictum-test-core/src/main/java/com/github/invictum/panels/FloatingPanel.java
+++ invictum-test-core/src/main/java/com/github/invictum/panels/FloatingPanel.java
@@ -1,24 +1,11 @@
 package com.github.invictum.panels;
 
 import com.github.invictum.unified.data.provider.UnifiedDataProviderUtil;
-import net.serenitybdd.core.pages.WebElementFacade;
-
-import java.util.List;
 
 public class FloatingPanel extends AbstractPanel {
 
     @Override
     protected String locatorValue(final String locatorKey) {
         return UnifiedDataProviderUtil.getLocatorByKey(locatorKey, dataProvider);
-    }
-
-    @Override
-    public WebElementFacade locate(final String locatorKey) {
-        return findBy(locator(locatorKey));
-    }
-
-    @Override
-    public List<WebElementFacade> locateAll(final String locatorKey) {
-        return findAll(locator(locatorKey));
     }
 }
