5d9c223dfc9b0ce5c081ee473d2bbebe7da5b6a2
Illia Grynov <eliogrin@gmail.com>
2016-10-24 13:16:27 +0200

    Updated: added 'invictum' prefix to all system properties (#70)

--- invictum-test-core/src/main/java/com/github/invictum/utils/properties/EnhancedSystemProperty.java
+++ invictum-test-core/src/main/java/com/github/invictum/utils/properties/EnhancedSystemProperty.java
@@ -2,15 +2,15 @@ package com.github.invictum.utils.properties;
 
 public enum EnhancedSystemProperty {
 
-    LocatorsDirectory("locators.directory", "locators"),
-    SchemasDirectory("rest.schemas.directory", "rest/schemas"),
-    FullDtoView("full.dto.view", "false"),
-    PagesPackageName("pages.package.name", "."),
-    PanelsPackageName("panels.package.name", "."),
-    FixturesPackageName("fixtures.package.name", "."),
-    DefaultUrlKey("default.url.key", "default"),
-    DataProviderParser("data.provider.parser", "com.github.invictum.unified.data.provider.parsers.YamlParser"),
-    PanelInitStrategy("panel.init.strategy", "com.github.invictum.panels.strategy.NoWaitStrategy");
+    LocatorsDirectory("invictum.locators.directory", "locators"),
+    SchemasDirectory("invictum.rest.schemas.directory", "rest/schemas"),
+    FullDtoView("invictum.full.dto.view", "false"),
+    PagesPackageName("invictum.pages.package.name", "."),
+    PanelsPackageName("invictum.panels.package.name", "."),
+    FixturesPackageName("invictum.fixtures.package.name", "."),
+    DefaultUrlKey("invictum.default.url.key", "default"),
+    DataProviderParser("invictum.data.provider.parser", "com.github.invictum.unified.data.provider.parsers.YamlParser"),
+    PanelInitStrategy("invictum.panel.init.strategy", "com.github.invictum.panels.strategy.NoWaitStrategy");
 
     private String key;
     private String defaultValue;
