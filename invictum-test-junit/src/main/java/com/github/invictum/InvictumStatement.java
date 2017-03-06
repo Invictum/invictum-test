faa2ffda142e01d8c39126fc893dec6d811586c8
Iarosalv Iershov <zim182@gmail.com>
2017-03-06 20:25:57 +0300

    [junit] Fixed: Session rollback in jUnit style tests #87 (#89)

---
+++ invictum-test-junit/src/main/java/com/github/invictum/InvictumStatement.java
@@ -0,0 +1,19 @@
+package com.github.invictum;
+
+import com.github.invictum.fixtures.FixtureProcessor;
+import org.junit.runners.model.Statement;
+
+public class InvictumStatement extends Statement {
+
+    private Statement statement;
+
+    public InvictumStatement(final Statement statement) {
+        this.statement = statement;
+    }
+
+    @Override
+    public void evaluate() throws Throwable {
+        statement.evaluate();
+        FixtureProcessor.rollback();
+    }
+}
