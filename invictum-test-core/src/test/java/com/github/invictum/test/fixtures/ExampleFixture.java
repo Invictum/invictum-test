39577fa245b6f99cc691d0dc98f0d495d975e600
Iaroslav Iershov <zim182@gmail.com>
2017-06-08 11:45:20 +0300

    Implemented: Fixture params rework #110  - added parameters method.  - marked getParam method as deprecated.  - updated relevant tests.

--- invictum-test-core/src/test/java/com/github/invictum/test/fixtures/ExampleFixture.java
+++ invictum-test-core/src/test/java/com/github/invictum/test/fixtures/ExampleFixture.java
@@ -2,6 +2,8 @@ package com.github.invictum.test.fixtures;
 
 import com.github.invictum.fixtures.AbstractFixture;
 
+import java.util.List;
+
 public class ExampleFixture extends AbstractFixture {
 
     @Override
@@ -12,5 +14,10 @@ public class ExampleFixture extends AbstractFixture {
     @Override
     public String getParam(int index) {
         return super.getParam(index);
+    }
+
+    @Override
+    public List<String> parameters() {
+        return super.parameters();
     }
 }
--- invictum-test-core/src/test/java/com/github/invictum/test/fixtures/FixtureTest.java
+++ invictum-test-core/src/test/java/com/github/invictum/test/fixtures/FixtureTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -20,25 +23,35 @@ public class FixtureTest {
 
     @Test
     public void paramTest() {
-        fixture.setParams(new String[] {"param1", "param2"});
+        fixture.setParams(new String[]{"param1", "param2"});
         assertThat("The first param is wrong.", fixture.getParam(0), equalTo("param1"));
         assertThat("The second param is wrong.", fixture.getParam(1), equalTo("param2"));
     }
 
     @Test
     public void paramAbsentTest() {
-        fixture.setParams(new String[] {"param"});
+        fixture.setParams(new String[]{"param"});
         assertThat("Parameter value with non-existing index is wrong.", fixture.getParam(1), equalTo(null));
     }
 
     @Test
+    public void parametersTest() {
+        List<String> list = new ArrayList<>();
+        list.add("param1");
+        list.add("param2");
+        String[] array = new String[list.size()];
+        fixture.setParams(list.toArray(array));
+        assertThat("Parameters list is wrong.", fixture.parameters(), equalTo(list));
+    }
+
+    @Test
     public void toStringTest() {
-        fixture.setParams(new String[] {"param1", "param2"});
+        fixture.setParams(new String[]{"param1", "param2"});
         assertThat("Fixture name is wrong.", fixture.toString(), equalTo("ExampleFixture {param1 param2}"));
     }
 
     @Test
     public void toStringNoParamTest() {
-        assertThat("Fixture name is wrong.", fixture.toString(), equalTo("ExampleFixture {}"));
+        assertThat("Fixture name is wrong.", fixture.toString(), equalTo("ExampleFixture"));
     }
 }
